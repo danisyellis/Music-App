7f49100c91a41487ad7dd87ec55ce1550d62bacc
Steve McDonald <steve@eastnorthwest.com>
2017-06-02 15:46:54 -0700

    Added postinstall script to reset database on install

---
+++ db/postinstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Running postinstall db setup for lg-filthy-flamingo-music-app on heroku\n\n";
+dropdb music_api --host=DATABASE_URL
+createdb music_api --host=DATABASE_URL
+psql -h DATABASE_URL music_api
+psql -h DATABASE_URL music_api < ./db/data.sql
