b5dac3b57f41c800fe2db7ac80c5c8ccaff5aaef
Steve McDonald <steve@eastnorthwest.com>
2017-06-02 16:14:04 -0700

    Heroku update

--- db/postinstall.sh
+++ db/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export `heroku config -s`
 echo "Running postinstall db setup for lg-filthy-flamingo-music-app on heroku";
+echo DB
 dropdb music_api --host=DATABASE_URL
 createdb music_api --host=DATABASE_URL
 psql -h DATABASE_URL music_api
