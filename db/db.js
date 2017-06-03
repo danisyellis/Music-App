9b22516f7cbb5f19f57ab7630ddacb4f82ac82c3
Steve McDonald <steve@eastnorthwest.com>
2017-06-03 06:56:07 -0700

    Updated db.js

--- db/db.js
+++ db/db.js
@@ -1,20 +1,37 @@
+const url = require('url');
 const pg = require('pg');
 
 // create a config to configure both pooling behavior
 // and client options
 // note: all config is optional and the environment variables
 // will be read if the config is not present
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
 
-const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  ssl: true
-};
+if (process.env.DATABASE_URL)
+{
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+} else {
+  const config = {
+    user: 'foo', //env var: PGUSER
+    database: 'music_api', //env var: PGDATABASE
+    password: '', //env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, //env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
+
+
 
 const pool = new pg.Pool(config);
 
